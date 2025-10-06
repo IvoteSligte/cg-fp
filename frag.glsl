25d474ccbb0bfcda7c96970ab91834bddc6b931c
Ivo te Sligte <ivotesligte@gmail.com>
2025-10-06 16:20:47 +0200

    fixed vertex/fragment variable linkage

--- frag.glsl
+++ frag.glsl
@@ -1,8 +1,8 @@
 #version 430
 
-in vec2 inPos;
+in vec2 fragPos;
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    fragColor = vec4(vec3(0.5), 1.0);
 }
--- vert.glsl
+++ vert.glsl
@@ -1,9 +1,9 @@
 #version 430
 
 in vec2 inPos;
-out vec2 outPos;
+out vec2 fragPos;
 
 void main() {
-    outPos = inPos;
+    fragPos = inPos;
     gl_Position = vec4(inPos, 0.0, 1.0);
 }
